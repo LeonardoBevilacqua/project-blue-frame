5535fcc2647232ab383ae43036874d1caec1fb06
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2021-11-12 13:23:14 -0300

    Fixing client url

--- client/src/services/application.service.ts
+++ client/src/services/application.service.ts
@@ -5,7 +5,7 @@ export class ApplicationService {
 
     constructor() {
         this.api = axios.create({
-            baseURL: 'http://server:5000' // TODO Add env
+            baseURL: 'http://0.0.0.0:5000' // TODO Add env
         })
     }
 
