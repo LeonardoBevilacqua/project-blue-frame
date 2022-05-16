a504d9953f12c14db9a747f139cdfad350166f53
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2022-05-16 17:02:21 -0300

    atempting to create a slide show

---
+++ src/routes/teste.svelte
@@ -0,0 +1,53 @@
+<!-- Slideshow container -->
+<div class="w-full relative m-auto">
+	<!-- Full-width images -->
+	<div class="">
+		<div class="text-gray-100 text-[12px] py-2 px-3 absolute top-0">1 / 4</div>
+		<img class="w-full" src="https://random.imagecdn.app/500/150" alt="teste 01" />
+		<div class="text-gray-100 text-[15px] py-2 px-3 absolute bottom-2 w-full text-center">
+			teste 01
+		</div>
+	</div>
+
+	<div class="hidden">
+		<div class="text-gray-100 text-[12px] py-2 px-3 absolute top-0">2 / 4</div>
+		<img class="w-full" src="https://random.imagecdn.app/500/150" alt="teste 02" />
+		<div class="text-gray-100 text-[15px] py-2 px-3 absolute bottom-2 w-full text-center">
+			teste 01
+		</div>
+	</div>
+
+	<div class="hidden">
+		<div class="text-gray-100 text-[12px] py-2 px-3 absolute top-0">3 / 4</div>
+		<img class="w-full" src="https://random.imagecdn.app/500/150" alt="teste 03" />
+		<div class="text-gray-100 text-[15px] py-2 px-3 absolute bottom-2 w-full text-center">
+			teste 01
+		</div>
+	</div>
+
+	<div class="hidden">
+		<div class="text-gray-100 text-[12px] py-2 px-3 absolute top-0">4 / 4</div>
+		<img class="w-full" src="https://random.imagecdn.app/500/150" alt="teste 04" />
+		<div class="text-gray-100 text-[15px] py-2 px-3 absolute bottom-2 w-full text-center">
+			teste 01
+		</div>
+	</div>
+
+	<!-- Next/Previous buttons -->
+	<button
+		class="absolute top-1/2 w-auto mt-[-22px] p-4 text-white font-bold text-[18px] rounded-tr-sm rounded-br-sm hover:bg-black/80"
+		>&#10094;</button
+	>
+	<button
+		class="absolute top-1/2 w-auto mt-[-22px] p-4 text-white font-bold text-[18px] rounded-tl-sm rounded-bl-sm hover:bg-black/80 right-0"
+		>&#10095;</button
+	>
+
+	<!-- Dots -->
+	<div class="text-center">
+		<span class="cursor-pointer h-4 w-4 mx-1 bg-gray-600 rounded-full inline-block hover:bg-gray-900" />
+		<span class="cursor-pointer h-4 w-4 mx-1 bg-gray-600 rounded-full inline-block hover:bg-gray-900" />
+		<span class="cursor-pointer h-4 w-4 mx-1 bg-gray-600 rounded-full inline-block hover:bg-gray-900" />
+		<span class="cursor-pointer h-4 w-4 mx-1 bg-gray-600 rounded-full inline-block hover:bg-gray-900" />
+	</div>
+</div>
